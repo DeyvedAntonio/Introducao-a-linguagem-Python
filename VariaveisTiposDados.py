5ddcf17cf36d223e42a0cd0d7e52f3a40c45ab4a
Deyved Antonio <deyved.antonio@gmail.com>
2021-03-30 06:07:07 -0300

    "Variáveis e tipos de dados - aula 09 do curso"

---
+++ VariaveisTiposDados.py
@@ -0,0 +1,9 @@
+#aula 09 - variáveis e tipos de dados
+nome = "deyved"
+numeroInteiro = 56
+f = 1.90
+minhalista = [1,2,"nome",5.8]
+print(type(nome))
+print(type(numeroInteiro))
+print(type(f))
+print(type(minhalista))
