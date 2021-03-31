4e4211da54055f51ac98cf6a356732aceb2df1f9
Deyved Antonio <deyved.antonio@gmail.com>
2021-03-31 06:51:02 -0300

    "Operadores relacionais - aula 10 do curso"

---
+++ OperadoresRelacionais.py
@@ -0,0 +1,16 @@
+#aula 10 - Operadores Relacionais
+a = 10
+b = 16
+c = 21
+if(a == b):
+    print("a é igual a b")
+if(a != b):
+    print("a é diferente de b")
+if(b >= a):
+    print("b é maior ou igual a a")
+if(b <= c):
+    print("b é menor ou igual a c")
+if(c > a):
+    print("c é maior que a")
+if(b < c):
+    print("b é menor que c")
