6ee8fa8f343f83b21662eec04637439e922348ed
Deyved Antonio <deyved.antonio@gmail.com>
2021-04-03 10:51:35 -0300

    Lista de exercício - questão 02

--- Exercicios.py
+++ Exercicios.py
@@ -4,3 +4,13 @@ if (idade >= 18):
     print("Você é maior de idade.")
 else:
     print("Você é menor de idade.")
+
+#questão 02
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2)/2
+if (media >= 6.0):
+    print("Aprovado")
+else:
+    print("Reprovado")
