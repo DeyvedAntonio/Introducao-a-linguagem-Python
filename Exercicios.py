00532aa55c0c0e50a64b82b741fc7366221effe3
Deyved Antonio <deyved.antonio@gmail.com>
2021-04-04 12:09:08 -0300

    Lista de exercício - questão 03

--- Exercicios.py
+++ Exercicios.py
@@ -1,16 +1,31 @@
-#questão 01 da lista 01
+import math
+# questão 01 da lista 01
 idade = int(input("Digite a sua idade: "))
-if (idade >= 18):
+if idade >= 18:
     print("Você é maior de idade.")
 else:
     print("Você é menor de idade.")
 
-#questão 02
+# questão 02
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 
 media = (nota1 + nota2)/2
-if (media >= 6.0):
+if media >= 6.0:
     print("Aprovado")
 else:
     print("Reprovado")
+
+# questão 03
+print("Equação do segundo grau: ax^2 + bx + c")
+a = int(input("Digite o valor de a: "))
+b = int(input("Digite o valor de b: "))
+c = int(input("Digite o valor de c: "))
+
+s = math.sqrt(b ** 2 - 4 * a * c)
+
+x1 = (-b + s)/(2 * a)
+x2 = (-b - s)/(2 * a)
+
+print("As raízes são: ")
+print(x1, x2)
